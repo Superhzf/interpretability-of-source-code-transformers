56e6d6e43cfd53e7e4bace8293adb353eff97380
Superhzf <superhzf@hotmail.com>
2022-08-07 21:18:12 -0500

    debug

--- POS Code/Visualization/run_neurox1.py
+++ POS Code/Visualization/run_neurox1.py
@@ -98,9 +98,9 @@ def visualization(bert_tokens, bert_activations,
     neuron=5
     if dev:
         for s_idx in range(len(bert_tokens["source"])):
-            this_svg = vis_bert.visualize_activations(bert_tokens["source"][s_idx],
-                                                      bert_activations[s_idx][:, neuron],
-                                                      filter_fn="top_tokens")
+            this_svg = vis.visualize_activations(bert_tokens["source"][s_idx],
+                                                 bert_activations[s_idx][:, neuron],
+                                                 filter_fn="top_tokens")
             # this_svg=viz_bert(bert_tokens["source"][s_idx], layer, neuron, filter_fn="top_tokens")
             this_svg.saveas(f"bert_{s_idx}_{layer}_{neuron}",pretty=True, indent=2)
             break
