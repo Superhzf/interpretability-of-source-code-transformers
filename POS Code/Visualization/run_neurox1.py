b2c5fdeb1f767a716e64fa3515003cb9b1fa2738
Superhzf <superhzf@hotmail.com>
2022-08-09 12:55:46 -0500

    top neurons

--- POS Code/Visualization/run_neurox1.py
+++ POS Code/Visualization/run_neurox1.py
@@ -106,8 +106,8 @@ def visualization(bert_tokens, bert_activations,
     else:
         layer = 0
         # starting from 1.
-        at_idx = [1, 5,7,8,9,10,11,12,13]
-        at_top_neurons = [2411]
+        at_idx = [46, 49,75,827]
+        at_top_neurons = [2946]
         for this_neuron in at_top_neurons:
             for this_idx in at_idx:
                 this_svg_bert = vis.visualize_activations(bert_tokens["source"][this_idx-1],
