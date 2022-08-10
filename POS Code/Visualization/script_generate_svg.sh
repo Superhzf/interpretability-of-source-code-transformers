f1be958190a9cb1f21ec0a8d73cbb5b4e9685662
Superhzf <superhzf@hotmail.com>
2022-08-10 10:22:39 -0500

    upgrade

--- POS Code/Visualization/script_generate_svg.sh
+++ POS Code/Visualization/script_generate_svg.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
-#SBATCH --mem=128G
-#SBATCH --time=12:00:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100-pcie:1
+#SBATCH --mem=8G
+#SBATCH --time=1:00:00
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Visualization
