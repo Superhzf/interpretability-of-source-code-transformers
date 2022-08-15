df0a7196ebd389c32bf6769cc84c94d032425b5c
Superhzf <superhzf@hotmail.com>
2022-08-15 11:50:34 -0500

    debug

--- POS Code/Visualization/generate_svg.py
+++ POS Code/Visualization/generate_svg.py
@@ -9,7 +9,7 @@ for this_neuron in bert_top_neurons:
     for this_idx in bert_idx:
         layer_idx = this_neuron//768
         neuron_idx = this_neuron%768
-        name = f"result/bert_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
+        this_name = f"result/bert_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
         bert_names.append(this_name)
 
 os.system(f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python \
@@ -23,7 +23,7 @@ for this_neuron in codebert_top_neurons:
     for this_idx in codebert_idx:
         layer_idx = this_neuron//768
         neuron_idx = this_neuron%768
-        name = f"result/codebert_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
+        this_name = f"result/codebert_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
         codebert_names.append(this_name)
 
 os.system(f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python \
@@ -37,7 +37,7 @@ for this_neuron in graphcodebert_top_neurons:
     for this_idx in graphcodebert_idx:
         layer_idx = this_neuron//768
         neuron_idx = this_neuron%768
-        name = f"result/graphcodebert_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
+        this_name = f"result/graphcodebert_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
         graphcodebert_names.append(this_name)
 
 os.system(f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python \
