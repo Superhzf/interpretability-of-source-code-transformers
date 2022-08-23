86d020bff5e5dcadc1e051f5f67969147d2bc9ce
arushi17 <arushi17@pronto.las.iastate.edu>
2022-08-23 16:54:56 -0500

    GraphCodeBERT DefDet

---
+++ Defect-detection/code/GraphCodeBert/finetuning.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem=128G
+#SBATCH --time=10-10:00:00
+#SBATCH --partition=gpu 
+#SBATCH --gres=gpu:a100-pcie:1
+module load ml-gpu
+
+cd /work/LAS/jannesar-lab/arushi/Redundancy/Defect-detection/code/GraphCodeBert
+
+ml-gpu /work/LAS/jannesar-lab/arushi/Environments/finetuning_env/bin/python run.py \
+    --output_dir=./saved_models \
+    --model_type=roberta \
+    --config_name=microsoft/graphcodebert-base \
+    --model_name_or_path=microsoft/graphcodebert-base \
+    --tokenizer_name=microsoft/graphcodebert-base \
+    --do_test \
+    --train_data_file=/work/LAS/jannesar-lab/arushi/Redundancy/Defect-detection/dataset/train.jsonl \
+    --eval_data_file=/work/LAS/jannesar-lab/arushi/Redundancy/Defect-detection/dataset/valid.jsonl \
+    --test_data_file=/work/LAS/jannesar-lab/arushi/Redundancy/Defect-detection/dataset/test.jsonl \
+    --epoch 2 \
+    --block_size 400 \
+    --train_batch_size 8 \
+    --eval_batch_size 16 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456 2>&1| tee train.log
