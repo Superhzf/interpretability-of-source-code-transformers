4def52f6e3bde25539056ea9dcdb8097234af2e2
Superhzf <superhzf@hotmail.com>
2022-09-01 15:09:18 -0500

    debug

--- POS Code/Experiments/analyze_neurons.py
+++ POS Code/Experiments/analyze_neurons.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from run_neurox1 import load_extracted_activations
 from run_neurox1 import load_tokens
+import neurox.analysis.corpus as corpus
 
 with open('log') as f:
     lines = f.read()
