9d3e4ffbdd42861721dfabcc0156e68c4278015b
Zefu Hu <zefuh@iastate.edu>
2022-09-14 23:34:21 +0000

    Obtain more data

--- POS Code/Preprocessing/skip_lines.py
+++ POS Code/Preprocessing/skip_lines.py
@@ -1,32 +1,38 @@
-import re
-import csv
+f = open('codetest.in','r')
+code = f.readlines()
+f.close()
+f = open('codetest.label','r')
+label = f.readlines()
+f.close()
 
-with open('skip_logs') as f_in:
-    lst = []
-    for line in f_in:
-        grp = re.search(r"Skipping line:\s+([0-9]+)",line)
-        #Skip if None; eg 23-27
-        if grp is None:
-            continue 
-        x = grp.groups() #returns tuples
-        #print(x)
-        lst.append(int(x[0])+1)
-    print(lst)
+assert len(code) == len(label)
 
-line_num = 0
-with open('codetest.in') as f_in, open('codetest2.in','w') as f_out:
-    for line in f_in.readlines():
-        line_num = line_num + 1
-        if line_num in lst:
-            continue
-        else:
-            f_out.writelines(line)
+# drop observations that the length of code does not
+# equal to the length of labels
+dropped_idx = 0
+f = open('codetest2.in','w')
+g = open('codetest2.label','w')
 
-line_num_l = 0
-with open ('codetest.label') as f_label, open('codetest2.label','w') as f_outl:
-    for line in f_label.readlines():
-        line_num_l = line_num_l + 1
-        if line_num_l in lst:
-            continue
-        else:
-            f_outl.writelines(line)
+for this_code, this_label in zip(code,label):
+    if len(this_code.split(" ")) != len(this_label.split(" ")):
+        dropped_idx+=1
+        continue
+    elif max([len(i) for i in this_code.split(" ")]) > 512:
+        dropped_idx+=1
+        continue
+    else:
+        f.writelines(this_code)
+        g.writelines(this_label)
+f.close()
+g.close()
+
+# SANITY CHECK: ensure the # of observations in both files are the same.
+with open("codetest2.in",'r') as f:
+    code = f.readlines()
+f.close()
+
+with open("codetest2.label",'r') as f:
+    label = f.readlines()
+f.close()
+
+assert len(code) == len(label)
