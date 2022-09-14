b61a2c93a6922504b4fbad15858cfefd53a1df07
Superhzf <superhzf@hotmail.com>
2022-09-14 11:50:10 -0500

    restore the file

--- POS Code/Experiments/run_neurox1.py
+++ POS Code/Experiments/run_neurox1.py
@@ -435,47 +435,6 @@ def main():
 
     bert_tokens, codebert_tokens, graphcodebert_tokens =  load_tokens(bert_activations, codebert_activations, graphcodebert_activations)
 
-    print("53:")
-    print(bert_tokens['source'][53-1])
-    print(bert_tokens['target'][53-1])
-    feature_matrix1 = bert_activations[53-1]
-
-    print("54:")
-    print(bert_tokens['source'][54-1])
-    print(bert_tokens['target'][54-1])
-    feature_matrix2 = bert_activations[54-1]
-    # diff = np.sum(feature_matrix1[:11] - feature_matrix2[:11],axis=1)
-    diff = feature_matrix1[:11][0] - feature_matrix2[:11][0]
-    print("Dimension of diff:",diff.shape)
-    with np.printoptions(threshold=np.inf):
-        print(repr(diff))
-
-    # print("96:")
-    # print(bert_tokens['source'][96-1])
-    # print(bert_tokens['target'][96-1])
-    # with np.printoptions(threshold=np.inf):
-    #     print(repr(bert_activations[96-1]))
-    #
-    # print("97:")
-    # print(bert_tokens['source'][97-1])
-    # print(bert_tokens['target'][97-1])
-    # with np.printoptions(threshold=np.inf):
-    #     print(repr(bert_activations[97-1]))
-    #
-    # print("98:")
-    # print(bert_tokens['source'][98-1])
-    # print(bert_tokens['target'][98-1])
-    # with np.printoptions(threshold=np.inf):
-    #     print(repr(bert_activations[98-1]))
-    #
-    # print("99:")
-    # print(bert_tokens['source'][99-1])
-    # print(bert_tokens['target'][99-1])
-    # with np.printoptions(threshold=np.inf):
-    #     print(repr(bert_activations[99-1]))
-    exit(0)
-
-
 
     with open("codetest2_unique.in",'r') as f:
         code = f.readlines()
--- POS Code/Experiments/script_main.sh
+++ POS Code/Experiments/script_main.sh
@@ -2,8 +2,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --mem=128G
-#SBATCH --time=1:00:00
+#SBATCH --time=5:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100-pcie:1
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Experiments
-ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1_temp.py --extract=False> log_test_temp
+ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --extract=False> log_test
