eb512c8b4f17ee4ad31c6751a63b02bb79adc54e
Superhzf <superhzf@hotmail.com>
2022-09-14 09:57:46 -0500

    experiment

--- POS Code/Experiments/run_neurox1.py
+++ POS Code/Experiments/run_neurox1.py
@@ -441,7 +441,8 @@ def main():
     print(bert_tokens['source'][54-1])
     print(bert_tokens['target'][54-1])
     feature_matrix2 = bert_activations[54-1]
-    diff = np.sum(feature_matrix1[:11] - feature_matrix2[:11],axis=1)
+    # diff = np.sum(feature_matrix1[:11] - feature_matrix2[:11],axis=1)
+    diff = feature_matrix1[:11][0] - feature_matrix2[:11][0]
     print("Dimension of diff:",diff.shape)
     with np.printoptions(threshold=np.inf):
         print(repr(diff))
