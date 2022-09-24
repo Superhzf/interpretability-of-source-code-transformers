b2c9277d7bde51b408e6158cb724b4350166e22a
Superhzf <superhzf@hotmail.com>
2022-09-24 16:46:17 -0500

    extend the ETA

--- POS Code/Visualization/script_main.sh
+++ POS Code/Visualization/script_main.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --mem=128G
-#SBATCH --time=5:00:00
+#SBATCH --time=10:00:00
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Visualization
