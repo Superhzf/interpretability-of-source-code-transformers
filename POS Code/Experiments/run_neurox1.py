6283214d28c4d309351fc2c14a864c672279ec2d
Superhzf <superhzf@hotmail.com>
2022-10-14 20:50:45 -0500

    type: debug

--- POS Code/Experiments/run_neurox1.py
+++ POS Code/Experiments/run_neurox1.py
@@ -410,7 +410,7 @@ def linear_probes_inference(tokens, activations,model_name):
     X, y, label2idx, idx2label, src2idx, idx2src = get_mappings(tokens,activations)
 
     X, y, label2idx, idx2label = filter_by_frequency(X,y,label2idx,idx2label,40,model_name)
-    break
+    exit(0)
 
     X_train, X_test, y_train, y_test = \
         train_test_split(X, y, test_size=0.1,random_state=50, shuffle=False)
