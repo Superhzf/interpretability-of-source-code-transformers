5402dcaac13774ec29ecd3a53cc4ed6e16957dfa
Superhzf <superhzf@hotmail.com>
2022-10-24 13:08:28 -0500

    type: feat Remove tokens that show up both in training and testing

--- POS Code/Experiments/run_neurox1.py
+++ POS Code/Experiments/run_neurox1.py
@@ -49,6 +49,45 @@ def main():
             _,_,flat_tokens_test,X_test, y_test, _, _=preprocess(ACTIVATION_NAMES[this_model][1],
                                             './src_files/codetest2_test_unique.in','./src_files/codetest2_test_unique.label',
                                             freq_threshold,this_model)
+            idx_selected = []
+            for this_token in flat_tokens_train:
+                if this_token in flat_tokens_test:
+                    idx_selected.append(False)
+                else:
+                    idx_selected.append(True)
+            assert len(idx_selected) == len(flat_tokens_train)
+
+            flat_tokens_train = flat_tokens_train[idx_selected]
+            X_train = X_train[idx_selected]
+            y_train = y_train[idx_selected]
+
+            new_tokens_train = []
+            new_activations_train = []
+            idx = 0
+            for this_tokens,this_activations in zip(tokens_train,activations_train):
+                this_new_tokens = []
+                this_new_activations = []
+                for this_token,this_activation in zip(this_tokens,this_activations):
+                    if idx_selected[idx]:
+                        this_new_tokens.append(this_token)
+                        this_new_activations.append(this_activation)
+                    idx+=1
+                if len(this_new_tokens)>0:
+                    this_new_tokens = np.array(this_new_tokens)
+                    this_new_activations.append(this_new_activations)
+                    new_tokens_train.append(this_new_tokens)
+                    new_activations_train.append(this_new_activations)
+            assert np.array([l for sublist in new_tokens_train for l in sublist]) == flat_tokens_train
+            assert len(np.array([l for sublist in new_activations_train for l in sublist])) == len(flat_tokens_train)
+            tokens_train = new_tokens_train
+            activations_train = new_activations_train
+            print("The distribution of classes after removing repeated tokens between training and tesing:")
+            print(Counter.collections(y_train))
+            print(label2idx_train)
+            exit(0)
+
+
+
             lookup_table = {1:2,2:3,3:1}
             for idx, this_y in enumerate(y_test):
                 if this_y in lookup_table:
