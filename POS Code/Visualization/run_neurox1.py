65e010b91433a4eb3c8518025eef54a34fd6620c
Superhzf <superhzf@hotmail.com>
2022-10-31 11:42:26 -0500

    type: refactor Change the neuron to generate SVG files

--- POS Code/Visualization/run_neurox1.py
+++ POS Code/Visualization/run_neurox1.py
@@ -21,8 +21,8 @@ ACTIVATION_NAMES = {'pretrained_BERT':'bert_activations_train.json',
                     'finetuned_clonedet_CodeBERT':'codebert_clonedet_activations1_train.json',
                     'finetuned_clonedet_GraphCodeBERT':'graphcodebert_clonedet_activations1_train.json'}
 # This set of idx is for pretrained, finetuned defdet, and finetuned clonedet models
-bert_idx = [3,4,5,6,7]
-bert_top_neurons = [4704]
+bert_idx = [3,4,5,19]
+bert_top_neurons = [2531]
 bert_class = "NAME"
 codebert_idx = None
 graphcodebert_idx = None
