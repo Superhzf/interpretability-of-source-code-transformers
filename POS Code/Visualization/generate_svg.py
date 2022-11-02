401ed58f9200cde7131908e048a5972a99647230
Superhzf <superhzf@hotmail.com>
2022-11-02 13:37:30 -0500

    type: debug

--- POS Code/Visualization/generate_svg.py
+++ POS Code/Visualization/generate_svg.py
@@ -10,7 +10,6 @@ def merge_svg(top_neurons,idx,class_name,model_name,folder_name):
             neuron_idx = this_neuron%768
             this_name = f"{model_name}_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
             names.append(this_name)
-            print("Bad")
 
     os.system(f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python \
                 svg_stack-main/svg_stack.py {folder_name}/{names[0]} {folder_name}/space.svg \
@@ -22,9 +21,8 @@ def merge_svg(top_neurons,idx,class_name,model_name,folder_name):
 
 for this_model in MODEL_NAMES:
     if this_model in ['pretrained_CodeBERT']:
-        print("Good")
         this_top_neuron = TOP_NEURONS[this_model]
         this_idx = IDX[this_model]
         this_class_name = CLASSES[this_model]
         merge_svg(this_top_neuron,this_idx,this_class_name,this_model,FOLDER_NAME)
-    break
+        break
