4683bb2a911304ac33303b3cca5ebee76ce15c7a
Superhzf <superhzf@hotmail.com>
2023-02-23 15:57:29 -0600

    type: refactor The max length of shared char is 2

--- POS Code/Experiments/utils.py
+++ POS Code/Experiments/utils.py
@@ -506,14 +506,14 @@ def selectBasedOnTrain(flat_tokens_test,X_test, y_test,flat_tokens_train,label2i
                     count_str += 1
             elif this_y_test == label2idx_train['NUMBER']:
                 for this_token_train in flat_tokens_train:
-                    if count_number>=280 or getOverlap(this_token_test,this_token_train) >= 1:
+                    if count_number>=280 or getOverlap(this_token_test,this_token_train) >= 2:
                         is_selected = False
                         break
                 if is_selected:
                     count_number += 1
             elif this_y_test == label2idx_train['NAME']:
                 for this_token_train in flat_tokens_train:
-                    if count_name>= 280 or getOverlap(this_token_test,this_token_train) >= 1:
+                    if count_name>= 280 or getOverlap(this_token_test,this_token_train) >= 2:
                         is_selected = False
                         break
                 if is_selected:
