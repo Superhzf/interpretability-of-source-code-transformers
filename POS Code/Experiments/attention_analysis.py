6734b739cba4405386ea877f106356ce8d2ea5f8
Superhzf <superhzf@hotmail.com>
2023-03-02 13:38:50 -0600

    type: debug

--- POS Code/Experiments/attention_analysis.py
+++ POS Code/Experiments/attention_analysis.py
@@ -4,5 +4,5 @@ from run_neurox1 import MODEL_NAMES,MODEL_DESC
 for this_model in MODEL_NAMES:
     if this_model in ['pretrained_BERT','pretrained_CodeBERT','pretrained_GraphCodeBERT']:
         print(f"Generating the attentions file for {this_model}")
-        extract_attentions('./src_files/temp.in',MODEL_DESC[this_model])
+        extract_attentions(MODEL_DESC[this_model],'./src_files/temp.in',)
         break
