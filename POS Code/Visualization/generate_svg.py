ac1fbcc46cf2506c7b121bfbd14ee5a99f5619f2
Superhzf <superhzf@hotmail.com>
2023-03-03 09:11:41 -0600

    type: feat merge svg files

--- POS Code/Visualization/generate_svg.py
+++ POS Code/Visualization/generate_svg.py
@@ -11,18 +11,20 @@ def merge_svg(top_neurons,idx,class_name,model_name,folder_name):
             this_name = f"{model_name}_{this_idx-1}_{layer_idx}_{neuron_idx}.svg"
             names.append(this_name)
 
-    # os.system(f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python \
-    #             svg_stack-main/svg_stack.py {folder_name}/{names[0]} {folder_name}/space.svg \
-    #             {folder_name}/{names[1]} {folder_name}/space.svg {folder_name}/{names[2]}> {folder_name}/{model_name}_{class_name}.svg")
     os.system(f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python \
                 svg_stack-main/svg_stack.py {folder_name}/{names[0]} {folder_name}/space.svg \
                 {folder_name}/{names[1]} {folder_name}/space.svg {folder_name}/{names[2]} \
                 {folder_name}/space.svg {folder_name}/{names[3]}> {folder_name}/{model_name}_{class_name}.svg")
 
+    command = f"/work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python svg_stack-main/svg_stack.py"
+    for this_name in names:
+        command = command + f" {folder_name}/{this_name} {folder_name}/space.svg"
+    command = command + f"> {folder_name}/{model_name}_{class_name}.svg"
+    os.system(command)
+
 for this_model in MODEL_NAMES:
-    if this_model in ['pretrained_CodeBERT']:
+    if this_model == 'pretrained_BERT':
         this_top_neuron = TOP_NEURONS[this_model]
         this_idx = IDX[this_model]
         this_class_name = CLASSES[this_model]
-        merge_svg(this_top_neuron,this_idx,this_class_name,this_model,FOLDER_NAME)
-        break
+        merge_svg(this_top_neuron,this_idx,this_class_name,this_model,'result_all/NUMBER')
--- POS Code/Visualization/run_neurox1.py
+++ POS Code/Visualization/run_neurox1.py
@@ -21,10 +21,10 @@ ACTIVATION_NAMES = {'pretrained_BERT':'bert_activations_train.json',
                     'finetuned_clonedet_CodeBERT':'codebert_clonedet_activations1_train.json',
                     'finetuned_clonedet_GraphCodeBERT':'graphcodebert_clonedet_activations1_train.json'}
 # This set of idx is for pretrained, finetuned defdet, and finetuned clonedet models
-# bert_idx = [147,182,178,182,198,2066,2149,4139,8161,8198,6758,6764]
-# bert_top_neurons = [2075]
+bert_idx = [147,182,178,182,198,2066,2149,4139,8161,8198,6758,6764]
+bert_top_neurons = [2075]
+bert_class = "NUMBER"
 
-# bert_class = "NUMBER"
 # bert_idx = [461,543,545,381,384,395,254,270,275,41,695,812,798]
 # bert_top_neurons = [4097]
 # bert_class = "NAME"
@@ -37,9 +37,9 @@ ACTIVATION_NAMES = {'pretrained_BERT':'bert_activations_train.json',
 # bert_top_neurons = [6295]
 # bert_class = "KEYWORD"
 
-bert_idx = [115,3621,1082,798]
-bert_top_neurons = [7]
-bert_class = "MIXTURE"
+# bert_idx = [115,3621,1082,798]
+# bert_top_neurons = [7]
+# bert_class = "MIXTURE"
 
 
 
