562ae4974d019bd80c69e9715d3f8ce7bc74dfd3
Superhzf <superhzf@hotmail.com>
2023-03-10 10:56:34 -0600

    type: refactor Refactor the regex to more efficiently extract tokens

--- POS Code/Preprocessing/POS_dataset_creation.py
+++ POS Code/Preprocessing/POS_dataset_creation.py
@@ -29,31 +29,41 @@ with open('codetest.label', 'a') as f_label, open('codetest.in', 'a') as f_in:
   with open('myfile_tokens.txt') as f_token:
       # lst = []
       for line in f_token:
-          grp = re.search(r"([0-9]+,[0-9]+-[0-9]+,[0-9]+:)\s*([A-Z]+)\s*('.*')\s*|[0-9]+,[0-9]+-[0-9]+,[0-9]+:\s+[A-Z]+\s+'.*'\s*",line)
-          #Skip if None; eg 23-27
-          if grp is None:
-              continue
-          x = grp.groups() #returns tuples
-          my_list = [x[1], x[2][1:-1]] #convert to mutable lists / strip quotes fro x[2]
-          #custom tokens for indent and dedent, identifying keywords
-          for item in (my_list):
-              if my_list[0] == 'INDENT':
-                  my_list[1] = '~~~'
-              if my_list[0] == 'DEDENT':
-                  my_list[1] = '~~'
-              if ((my_list[0] == 'NAME') and (my_list[1] in keyword_list)):
-                  my_list[0] = 'KEYWORD'
-          # lst.append(my_list) #store in list of lists
+        #   grp = re.search(r"([0-9]+,[0-9]+-[0-9]+,[0-9]+:)\s*([A-Z]+)\s*('.*')\s*|[0-9]+,[0-9]+-[0-9]+,[0-9]+:\s+[A-Z]+\s+'.*'\s*",line)
+        grp1 = re.search(r"""([0-9]+,[0-9]+-[0-9]+,[0-9]+:)\s*([A-Z]+)\s*[']{1}(.*?)[']{1}""",line)
+        grp2 = re.search(r"""([0-9]+,[0-9]+-[0-9]+,[0-9]+:)\s*([A-Z]+)\s*["]{1}(.*?)["]{1}""",line)
+        if grp1 is not None and grp2 is not None:
+            if len(grp1[2])>len(grp2[2]):
+                grp = grp1
+            else:
+                grp = grp2
+        elif grp1 is not None:
+            grp = grp1
+        elif grp2 is not None:
+            grp = grp2
+        else:
+            assert 1==0, print(f"Something is wrong with, {i}, {line}")
+        x = grp.groups()
+        my_list = [x[1], x[2]]
+        for item in (my_list):
+            if my_list[0] == 'INDENT':
+                my_list[1] = '~~~'
+            elif my_list[0] == 'DEDENT':
+                my_list[1] = '~~'
+            elif ((my_list[0] == 'NAME') and (my_list[1] in keyword_list)):
+                my_list[0] = 'KEYWORD'
+        lst.append(my_list)
+        # lst.append(my_list) #store in list of lists
 
-          if (my_list[0] == 'NEWLINE' or my_list[0] == 'NL'):
+        if (my_list[0] == 'NEWLINE' or my_list[0] == 'NL'):
             f_label.writelines("\n")
             f_in.writelines("\n")
-          elif (my_list[0] == 'ENDMARKER'):
+        elif (my_list[0] == 'ENDMARKER'):
             continue
-          else:
+        else:
             f_label.writelines(my_list[0] + ' ')
             f_in.writelines(my_list[1]+ ' ' )
-        
+    
 
 
 f_in.close()
