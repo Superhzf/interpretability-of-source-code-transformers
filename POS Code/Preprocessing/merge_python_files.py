4a08b86c5f5abf80c188a161ce0f6468ea25d37a
Superhzf <superhzf@hotmail.com>
2023-03-10 11:30:17 -0600

    type: feat Generate IN and LABEL files for validation set

--- POS Code/Preprocessing/merge_python_files.py
+++ POS Code/Preprocessing/merge_python_files.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 
 files = pd.read_pickle(r'deduplicated_python_code.pickle')
-files = files[:5000] # Train
-# files = files[3000:4000] # Dev
-# files = files[4000:5000] # Test
+# files = files[:5000] # Train
+files = files[5000:8000] # valid
+# files = files[8000:11000] # Test
 
 my_file = "myfile.txt"
 
