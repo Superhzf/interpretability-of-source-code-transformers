be7fee7a8550c71a9417f7c79eebe7e2304a65b9
Zefu Hu <zefuh@iastate.edu>
2023-03-21 19:23:19 -0500

    type: feat Change the restriction about the length of overlapping.

--- POS Code/Experiments/run_neurox1.py
+++ POS Code/Experiments/run_neurox1.py
@@ -142,7 +142,7 @@ def main():
                                                         label2idx_train,
                                                         keyword_list_valid,
                                                         num_valid,
-                                                        540)
+                                                        1000)
             print(f"Write tokens in the validation set to files:")
             f = open('validation.txt','w')
             for this_token in flat_tokens_valid:
@@ -156,7 +156,7 @@ def main():
                                                                                     label2idx_train,
                                                                                     keyword_list_test,
                                                                                     num_test,
-                                                                                    660,
+                                                                                    1000,
                                                                                     sample_idx_test)
             print(f"Write tokens in the testing set to files:")
             f = open('testing.txt','w')
@@ -176,6 +176,8 @@ def main():
             print("The distribution of classes in testing:")
             print(collections.Counter(y_test))
             print(label2idx_test)
+
+            exit(0)
 
             neurons_per_layer = X_train.shape[1]//num_layers
             assert neurons_per_layer==X_train.shape[1]/num_layers, f"Model:{this_model},Something is wrong with either number of layers={num_layers} or total neurons={X_train.shape[1]}"
--- POS Code/Experiments/utils.py
+++ POS Code/Experiments/utils.py
@@ -579,7 +579,7 @@ def selectBasedOnTrain(flat_tokens_test,X_test, y_test,flat_tokens_train,label2i
                 # because it is possible that they are different but very similar. If that is the case,
                 # it is highly likely that the the label would be the same.
                 for this_token_train in flat_tokens_train:
-                    if count_str>=upper_bound or getOverlap(this_token_test,this_token_train) >= 4:
+                    if count_str>=upper_bound:
                         is_selected = False
                         break
                 if is_selected:
@@ -591,7 +591,7 @@ def selectBasedOnTrain(flat_tokens_test,X_test, y_test,flat_tokens_train,label2i
                     count_number += 1
             elif this_y_test == label2idx_train['NAME']:
                 for this_token_train in flat_tokens_train:
-                    if count_name>= upper_bound or getOverlap(this_token_test,this_token_train) >= 2:
+                    if count_name>= upper_bound:
                         is_selected = False
                         break
                 if is_selected:
