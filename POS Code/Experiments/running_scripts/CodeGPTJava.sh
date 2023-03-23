36ad5a2babee20f21b3d99a48c9011cbaeadbbbc
Zefu Hu <zefuh@iastate.edu>
2023-03-22 20:41:24 -0500

    type: feat Better organize the log files to make it more clear

--- POS Code/Experiments/running_scripts/CodeGPTJava.sh
+++ POS Code/Experiments/running_scripts/CodeGPTJava.sh
@@ -6,4 +6,4 @@
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Experiments
-ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --extract=False --this_model=pretrained_codeGPTJava > logCodeGPTJava
+ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --extract=False --this_model=pretrained_codeGPTJava > CodeGPTJava.log
