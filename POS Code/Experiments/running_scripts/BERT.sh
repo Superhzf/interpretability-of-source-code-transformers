13c453e6ffaa12cca2013007d95eff99c33b0aee
Zefu Hu <zefuh@iastate.edu>
2023-03-24 00:22:11 -0500

    type: feat Use speedy nodes

--- POS Code/Experiments/running_scripts/BERT.sh
+++ POS Code/Experiments/running_scripts/BERT.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
-#SBATCH --mem=128G
+#SBATCH --mem=96G
 #SBATCH --time=3:00:00
+#SBATCH --partition=speedy
+
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Experiments
-ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --extract=False > BERT.log
+ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --extract=False > ./running_scripts/BERT.log
