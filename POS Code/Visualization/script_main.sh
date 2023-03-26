767487b8dd58e587c4216f4f4d941da76aeb1ed7
zefuh <zefuh@iastate.edu>
2023-03-25 19:13:29 -0500

    type: feat Change the required setups

--- POS Code/Visualization/script_main.sh
+++ POS Code/Visualization/script_main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
-#SBATCH --mem=128G
-#SBATCH --time=10:00:00
+#SBATCH --mem=64G
+#SBATCH --time=1:00:00
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Visualization
