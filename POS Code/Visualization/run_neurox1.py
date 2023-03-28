90507feb7e675d0ed2088b6b301bc988a1227deb
Zefu Hu <zefuh@iastate.edu>
2023-03-28 17:14:19 -0500

    type: debug

--- POS Code/Visualization/run_neurox1.py
+++ POS Code/Visualization/run_neurox1.py
@@ -44,7 +44,7 @@ bert_class =''
 # codebert_top_neurons = [1069]
 # codebert_class = "MIXTURE_NUMBER_NAME"
 
-codebert_idx=[5272,8679,11604,25972,25813]
+codebert_idx=[3678,8679,11604,25972,25813]
 codebert_top_neurons = [1652]
 codebert_class = "STRING_KEYWORD"
 # 1652
