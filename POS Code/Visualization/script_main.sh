821e5aa2ab6f50789c522d1c2b2be256217de729
Zefu Hu <zefuh@iastate.edu>
2023-03-28 13:20:27 -0500

    type: feat Use the speedy node for SVG generation

--- POS Code/Visualization/script_main.sh
+++ POS Code/Visualization/script_main.sh
@@ -3,6 +3,7 @@
 #SBATCH --ntasks=8
 #SBATCH --mem=64G
 #SBATCH --time=1:00:00
+#SBATCH --partition=speedy
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Visualization
