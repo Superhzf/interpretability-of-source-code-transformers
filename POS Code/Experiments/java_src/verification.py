ed3a0a3dd7db9d575952a79a1050e99ee98825b6
Superhzf <superhzf@hotmail.com>
2023-04-11 19:25:16 -0500

    type: feat Split the files into train valid and test

--- POS Code/Experiments/java_src/verification.py
+++ POS Code/Experiments/java_src/verification.py
@@ -40,8 +40,7 @@ f.close()
 
 assert len(code) == len(label)
 
-OUTPUT_IN = "codetest2_test_unique.in"
-OUTPUT_LABEL = "codetest2_test_unique.label"
+
 FOLDER = './'
 
 code_unique = []
@@ -52,14 +51,46 @@ for this_code, this_label in zip(code, label):
         label_unique.append(this_label)
 
 assert len(code_unique) == len(label_unique)
-with open(os.path.join(FOLDER, OUTPUT_IN),"w") as f:
-    for this_code in code_unique:
-        f.writelines(f"{this_code}")
-f.close()
 
-with open(os.path.join(FOLDER, OUTPUT_LABEL),"w") as f:
-    for this_label in label_unique:
-        f.writelines(f"{this_label}")
-f.close()
-print(f"After removing redundant observations, {len(code_unique)} samples are\
-left and written to the {OUTPUT_IN} and {OUTPUT_LABEL} files")
+per_train = 0.5
+OUTPUT_IN_TRAIN = "codetest2_train_unique.in"
+OUTPUT_LABEL_TRAIN = "codetest2_train_unique.label"
+per_valid = 0.25
+OUTPUT_IN_VALID = "codetest2_valid_unique.in"
+OUTPUT_LABEL_VALID = "codetest2_valid_unique.label"
+per_test = 0.25
+OUTPUT_IN_TEST = "codetest2_test_unique.in"
+OUTPUT_LABEL_TEST = "codetest2_test_unique.label"
+
+per = [per_train,per_valid,per_test]
+files = [(OUTPUT_IN_TRAIN,OUTPUT_LABEL_TRAIN),(OUTPUT_IN_VALID,OUTPUT_LABEL_VALID),(OUTPUT_IN_TEST,OUTPUT_LABEL_TEST)]
+
+count = 0
+for idx,this_per,this_file in enumerate(zip(per,files)):
+    if idx == 0:
+        end = int(len(code_unique) * this_per)
+        this_code = code_unique[:end]
+        this_label = label_unique[:end]
+    elif idx != len(per) - 1:
+        start = int(len(code_unique) * this_per)
+        end = int(len(code_unique) * per[idx+1])
+        this_code = code_unique[start:end]
+        this_code = label_unique[start:end]
+    else:
+        start = int(len(code_unique) * this_per)
+        this_code = code_unique[start:]
+        this_label = label_unique[start:]
+
+    count += len(this_label)
+    with open(os.path.join(FOLDER, files[0]),"w") as f:
+        for this_code in code_unique:
+            f.writelines(f"{this_code}")
+    f.close()
+
+    with open(os.path.join(FOLDER, files[1]),"w") as f:
+        for this_label in label_unique:
+            f.writelines(f"{this_label}")
+    f.close()
+
+print(f"After removing redundant observations, {count} samples are\
+left and written to files")
