902465c9fe52d2aad8533e2a7d713a0468d69bd2
zefuh <zefuh@iastate.edu>
2023-04-23 17:36:23 -0500

    type: debug

--- POS Code/Visualization/run_neurox1.py
+++ POS Code/Visualization/run_neurox1.py
@@ -20,9 +20,10 @@ ACTIVATION_NAMES = {'pretrained_BERT':'bert_activations_train.json',
                     'finetuned_clonedet_GraphCodeBERT':'graphcodebert_clonedet_activations1_train.json'}
 # This set of idx is for pretrained, finetuned defdet, and finetuned clonedet models
 
-codebert_idx=[5409,27486,30012,8454,6620]
+codebert_idx=[5408,27485,30011,8453,6619]
 codebert_top_neurons = [8418]
 codebert_class = "MIXTURE_NUMBER_NAME"
+# 6426
 
 # codebert_idx=[3678,8679,11604,25972,25813]
 # codebert_top_neurons = [1652]
