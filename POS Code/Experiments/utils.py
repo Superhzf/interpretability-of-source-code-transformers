51ed7c0686fac927256a917d35b28b4e11f605ac
zefuh <zefuh@iastate.edu>
2023-05-08 19:42:22 -0500

    type: feat Early stop is disabled, but add X_valid and y_valid and patience.

--- POS Code/Experiments/utils.py
+++ POS Code/Experiments/utils.py
@@ -111,10 +111,11 @@ def param_tuning(X_train,y_train,X_valid,y_valid,idx2label,l1,l2):
     for this_l1 in l1:
         for this_l2 in l2:
             this_probe = linear_probe.train_logistic_regression_probe(X_train, y_train,
+                                                                    X_valid, y_valid,
                                                                     lambda_l1=this_l1,
                                                                     lambda_l2=this_l2,
                                                                     num_epochs=10,
-                                                                    batch_size=128)
+                                                                    batch_size=128,patience=2)
             this_score = linear_probe.evaluate_probe(this_probe, X_valid, y_valid, idx_to_class=idx2label)
             if this_score['__OVERALL__'] > best_score:
                 best_score_valid = this_score['__OVERALL__']
