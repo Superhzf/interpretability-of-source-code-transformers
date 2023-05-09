17634b39556f84cfc32dc375e12c7587d62a785c
Superhzf <superhzf@hotmail.com>
2023-05-09 16:51:00 -0500

    type: feat Use speedy

--- POS Code/Experiments/running_scripts_java/CodeGPTPy.sh
+++ POS Code/Experiments/running_scripts_java/CodeGPTPy.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=8
 #SBATCH --mem=72G
 #SBATCH --time=3:00:00
-#SBATCH --partition=swift
+#SBATCH --partition=speedy
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Experiments
