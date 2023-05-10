0a24b1972fed175ce64a14c96c38661348dc9a59
zefuh <zefuh@iastate.edu>
2023-05-09 21:31:26 -0500

    type: feat Seed=0,batchsize=1024,early stop

--- POS Code/Experiments/utils.py
+++ POS Code/Experiments/utils.py
@@ -114,7 +114,7 @@ def param_tuning(X_train,y_train,X_valid,y_valid,idx2label,l1,l2):
                                                                     X_valid, y_valid,
                                                                     lambda_l1=this_l1,
                                                                     lambda_l2=this_l2,
-                                                                    num_epochs=50,
+                                                                    num_epochs=100,
                                                                     batch_size=1024,patience=2)
             this_score = linear_probe.evaluate_probe(this_probe, X_valid, y_valid, idx_to_class=idx2label)
             if this_score['__OVERALL__'] > best_score_valid:
