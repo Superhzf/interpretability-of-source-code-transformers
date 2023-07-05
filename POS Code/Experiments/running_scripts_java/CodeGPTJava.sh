2443c5e4c522af7ec864505d4d98eda6eb7f27ee
zefuh <zefuh@iastate.edu>
2023-07-05 12:22:22 -0500

    type: feat Use speedy node

--- POS Code/Experiments/running_scripts_java/CodeGPTJava.sh
+++ POS Code/Experiments/running_scripts_java/CodeGPTJava.sh
@@ -3,8 +3,7 @@
 #SBATCH --ntasks=8
 #SBATCH --mem=72G
 #SBATCH --time=36:00:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100-sxm4-80gb:1
+#SBATCH --partition=speedy
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Experiments
