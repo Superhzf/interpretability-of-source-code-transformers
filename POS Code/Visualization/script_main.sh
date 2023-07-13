8301bfccc0813d8d43a51c84acbd42ce3ce71e7c
zefuh <zefuh@iastate.edu>
2023-07-13 12:34:07 -0500

    type:feat Generate log

--- POS Code/Visualization/script_main.sh
+++ POS Code/Visualization/script_main.sh
@@ -7,4 +7,4 @@
 
 module load ml-gpu
 cd /work/LAS/cjquinn-lab/zefuh/selectivity/interpretability-of-source-code-transformers/POS\ Code/Visualization
-ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --language java
+ml-gpu /work/LAS/cjquinn-lab/zefuh/selectivity/NeuroX_env/bin/python run_neurox1.py --language java > log
