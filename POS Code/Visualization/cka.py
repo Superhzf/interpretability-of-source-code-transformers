5c631ffa6e5665102dcfd102e203f1ca8789a53c
Superhzf <superhzf@hotmail.com>
2023-07-23 20:02:23 -0500

    type:debug

--- POS Code/Visualization/cka.py
+++ POS Code/Visualization/cka.py
@@ -3,6 +3,7 @@ import neurox.data.loader as data_loader
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import axes_grid1
 
 MODEL_NAMES = ['pretrained_BERT',
                'pretrained_CodeBERT','pretrained_GraphCodeBERT',]
@@ -39,6 +40,17 @@ def HSIC(K, L):
         result += (ones.transpose() @ K @ ones @ ones.transpose() @ L @ ones) / ((N - 1) * (N - 2))
         result -= (ones.transpose() @ K @ L @ ones) * 2 / (N - 2)
         return 1 / (N * (N - 3)) * result[0,0]
+
+
+def add_colorbar(im, aspect=10, pad_fraction=0.5, **kwargs):
+    """Add a vertical color bar to an image plot."""
+    divider = axes_grid1.make_axes_locatable(im.axes)
+    width = axes_grid1.axes_size.AxesY(im.axes, aspect=1./aspect)
+    pad = axes_grid1.axes_size.Fraction(pad_fraction, width)
+    current_ax = plt.gca()
+    cax = divider.append_axes("right", size=width, pad=pad)
+    plt.sca(current_ax)
+    return im.axes.figure.colorbar(im, cax=cax, **kwargs)
 
 
 def plot_results(hsic_matrix,save_path,title):
