5d53b2512accddf4b2ee3a0a3915661125b6fb48
Superhzf <superhzf@hotmail.com>
2023-07-29 13:58:39 -0500

    type:feat run full experiment

--- POS Code/Visualization/cka.py
+++ POS Code/Visualization/cka.py
@@ -29,7 +29,7 @@ ACTIVATION_NAMES_sentence_level = {'BERT':'bert/train_activations.json',
 N_LAYERs = 13
 N_NEUROSN_PER_LAYER = 768
 N_SAMPLES = 5000
-N_BATCHES = 1
+N_BATCHES = 5
 
 def mkdir_if_needed(dir_name):
     if not os.path.isdir(dir_name):
@@ -167,7 +167,7 @@ def main():
         del activations
         plot_results(hsic_matrix,save_path=f"{this_model}_cka.png",title=this_model)
         print("-----------------------------------------------------------------")
-        break
+
 
 if __name__ == "__main__":
     main()
