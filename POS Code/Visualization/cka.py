9907a4974114a8ffb53ea21dc1aecb127d3afdac
Superhzf <superhzf@hotmail.com>
2023-07-28 20:58:12 -0500

    type:feat print K

--- POS Code/Visualization/cka.py
+++ POS Code/Visualization/cka.py
@@ -104,9 +104,11 @@ def cka(activation1,n_samples):
             index = i*N_NEUROSN_PER_LAYER
             this_X = this_sample[:,index:index+N_NEUROSN_PER_LAYER]
             print("this_X",this_X)
-            exit(0)
+            
             # The dimension is seq_len X 9984
             K = this_X @ this_X.transpose()
+            print("K",K)
+            exit(0)
             np.fill_diagonal(K,0.0)
             hsic_matrix[i, :, 0] += HSIC(K, K) / num_batches
 
